9fdc9085a4bd522e36a94e212562e727f23fa75e
Maksim Siarheichyk <maksim_siarheichyk@epam.com>
2017-11-22 14:34:14 +0300

    Added: work with server. write data

---
+++ Client_Side/Client_Side/WorkWithServer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Client_Side
+{
+    class WorkWithServer
+    {
+        private static WebClient cl = new WebClient();
+
+        public static void sendData(string URL, string method, string data)
+        {
+            try
+            {
+                cl.UploadData(URL, method, Encoding.Default.GetBytes(data));
+            }
+            catch (Exception ex)
+            {
+                Program.WriteLog(ex.Message);
+            }
+        }
+
+        public static string getData(string URL, string query)
+        {
+            string response = cl.DownloadString(URL + Uri.EscapeUriString(query));
+            return response;
+        }
+    }
+}
---
+++ Client_Side/Client_Side/WriteData.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Client_Side
+{
+    class WriteData
+    {
+        private WriteData() { }
+
+
+        #region Data
+
+        private static FileStream fs;
+
+        private static string Url;
+
+        private static string WriteMode;
+
+        private static string PathToFile;
+
+        private static bool IsFile = false;
+
+
+        #endregion
+
+        #region Methods
+
+        public static void SendData(List<string> data)
+        {
+            if ((WriteMode == "0") || (WriteMode == "2"))
+            {
+                if (!IsFile)
+                {
+                    CreateResultFile();
+                }
+            }
+            try
+            {
+                switch (WriteMode)
+                {
+                    case "0": { WriteDataInFile(data); break; }
+                    case "1": { WriteDataToInfluxDB(data); break; }
+                    case "2": { WriteDataInFile(data); WriteDataToInfluxDB(data); break; }
+                }
+            }
+            catch
+            { }
+        }
+
+        public static bool PingServerAndPort()
+        {
+            return PingUtil.Ping(Url, false);
+        }
+
+        private static void CreateResultFile()
+        {
+            string time = String.Format("{0:yyyyddMM_HHmmss}", DateTime.Now).ToString();
+            string path = PathToFile + time + ".txt";
+            fs = File.Create(path);
+            IsFile = true;
+        }
+
+        private static void WriteDataToInfluxDB(List<string> data)
+        {
+            try
+            {
+                data.ForEach(i => WorkWithServer.sendData(Url, "Post", i));
+            }
+            catch (Exception ex)
+            {
+                Program.WriteLog(ex.Message);
+            }
+        }
+
+        private static void WriteDataInFile(List<string> data)
+        {
+            data.ForEach(i => { byte[] ar = new UTF8Encoding(true).GetBytes(i + "\n"); fs.Write(ar, 0, ar.Length); });
+        }
+
+        #endregion
+
+        #region Properties
+
+        public static string SetUrl
+        {
+            set
+            {
+                Url = value;
+            }
+        }
+
+        public static string SetDBName
+        {
+            set
+            {
+                Url += "/write?db=" + value + "&precision=ms";
+            }
+        }
+
+        public static string SetWriteMode
+        {
+            set
+            {
+                WriteMode = value;
+            }
+        }
+
+        public static string SetPathToFile
+        {
+            set
+            {
+                PathToFile = value;
+            }
+        }
+
+        #endregion
+    }
+}
