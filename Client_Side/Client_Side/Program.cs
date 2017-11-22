fd45c59954f05cb4df3c1cc36e33242bd1a96ccf
Maksim Siarheichyk <maksim_siarheichyk@epam.com>
2017-11-22 14:25:59 +0300

    WebDriver added.

--- Client_Side/Client_Side/Program.cs
+++ Client_Side/Client_Side/Program.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace Client_Side
 {
     class Program
     {
+        public static void ShowMessage(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+        public static void WriteLog(string s)
+        {
+            File.AppendAllText(@"D:/ClientSideLog.txt", s + "\n");
+        }
+
         static void Main(string[] args)
         {
         }
---
+++ Client_Side/Client_Side/WebDriver.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+
+namespace Client_Side
+{
+    //Singleton implementation for IWebDriver
+
+    class WebDriver
+    {
+        private WebDriver() { }
+
+        private static IWebDriver driver;
+
+        public static string browser;
+
+        public static IWebDriver Driver
+        {
+            get
+            {
+                if (driver==null)
+                {
+                    switch(browser)
+                    {
+                        case "chrome":
+                            {
+                                ChromeOptions options = new ChromeOptions();
+                                options.AddArgument("--start-maximized");
+
+                                options.AddUserProfilePreference("cacheDisabled", true);
+
+                                driver = new ChromeDriver(options);
+                                driver.Manage().Cookies.DeleteAllCookies();
+                                break;
+                            }
+                        case "firefox":
+                            {
+                                driver = new FirefoxDriver();
+                                driver.Manage().Cookies.DeleteAllCookies();
+                                break;
+                            }
+                    }
+                }
+                return driver;
+            }
+        }
+
+        public static void Close()
+        {
+            driver.Close();
+            driver = null;
+        }
+    }
+}
